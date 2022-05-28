7a7a27f0b143e7836776f7de0aadaacc99a10613
jubedoyag <jubedoyag@unal.edu.co>
2022-05-27 22:04:00 -0500

    merge con la rama deletePublication

--- Server/routes/deletePublicationRoute.js
+++ Server/routes/deletePublicationRoute.js
@@ -5,13 +5,8 @@ const express = require('express');
 const router = express.Router();
 const deletePublication = require('../models/deletePublication')
 
-<<<<<<< HEAD
-// La dirección '/' es relativa a '/delete'. Si fuera '/url', la dirección absoluta sería '/delete/url'
-router.delete('/', async (req, res) => {
-=======
 // La dirección '/' es relativa a '/delete-post'. Si fuera '/url', la dirección absoluta sería '/delete-post/url'
 router.delete('/', async (req, res) => {// validacion ?
->>>>>>> 98c813fe4008bd842be1688861a1a0955c1e746c
 
     try{
         await deletePublication(req.body.id)
