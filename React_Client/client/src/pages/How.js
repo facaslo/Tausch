45de0177e64cd5411048407272e488c58f7acc53
yybeltranr <yybeltranr@unal.edu.co>
2022-05-30 02:43:38 -0500

    Creación página quienes somos.

--- React_Client/client/src/pages/How.js
+++ React_Client/client/src/pages/How.js
@@ -5,10 +5,8 @@ function How () {
     return(
         <>
             <div className="general-container">
-                <div className="page-container">
                 <h1>Cómo funciona</h1>
                     Publica los items o servicios que deseas intercambiar y espera a que alguien ofrezca algo que te interese o también puedes ser tú quien vaya a la busca de artículos de tu interés. En cualquier caso, Tausch te facilitará ponerte en contacto con la otra persona.
-                </div>
             </div>
         </>
     );
--- React_Client/client/src/pages/Index.js
+++ React_Client/client/src/pages/Index.js
@@ -1,19 +1,13 @@
 import React from "react";
 import './General-container.css'
-import Gallery from "../components/Gallery";
 import PublicationsAll from "../components/PublicationsAll";
 
 function Home () {
     return(
         <>
             <div className="general-container">
-                <div className="page-container">
-                    {/*<div className="flex justify-content-center">
-                        < Gallery />
-                </div>*/}
-                    <div className="flex justify-content-center">
-                        < PublicationsAll />
-                    </div>
+                <div className="flex justify-content-center">
+                    < PublicationsAll />
                 </div>
             </div>            
         </>
