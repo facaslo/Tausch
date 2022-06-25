89ddb2f1c13b42764b2080268744b986bd78faa7
yybeltranr <yybeltranr@unal.edu.co>
2022-06-25 02:49:19 -0500

    Cambios visuales textos

--- React_Client/client/src/pages/How.js
+++ React_Client/client/src/pages/How.js
@@ -3,7 +3,6 @@ import { Image } from 'primereact/image';
 import './General-container.css'
 
 function How () {
-    let x = 1;
     return(
         <>
             <div className="general-container">
@@ -18,21 +17,18 @@ function How () {
                         <div class="col-md-8">
                             <div className="card-body mt-3 m-2">
                                 <h5 className="card-text text-justify">
-                                    <span className="capitalLetter">T</span>ausch, 
-                                    <div className="waviy">
-                                        {/*}
-                                        <span style="--i:1">t</span><span style="--i:2">u </span>
-                                        <span style="--i:3">l</span><span style="--i:4">l</span><span style="--i:5">a</span><span style="--i:6">v</span><span style="--i:7">e </span>
-                                        <span style="--i:8">a </span>
-                                        <span style="--i:9">l</span><span style="--i:10">o </span>
-                                        <span style="--i:11">q</span><span style="--i:12">u</span><span style="--i3">e </span>
-                                        <span style="--i:14">b</span><span style="--i:15">u</span><span style="--i:16">s</span><span style="--i:17">c</span><span style="--i:18">a</span><span style="--i:19">s</span>*/}
-                                        {/*<span style={{ animationDelay: --i }} > t</span>
-                                        <span style={{ animationDelay: --i+1 }} > u</span>*/}
-                                        <span style={{ var:1}} > y </span>
-                                        <span style={{ var:2 }} > u</span>
-                                    </div>
-                                    tu llave a lo que buscas,  es una aplicación web que te permite intercambiar artículos que ya no usas
+                                    <span className="capitalLetter">T</span>ausch,
+                                    <span className="textBlur">
+                                        <strong>
+                                        <span>&nbsp;tu&nbsp;</span>
+                                        <span>llave&nbsp;</span>
+                                        <span>a&nbsp;</span>
+                                        <span>lo&nbsp;</span>
+                                        <span>que&nbsp;</span>
+                                        <span>buscas</span>
+                                        </strong>
+                                </span>
+                                    ,  es una aplicación web que te permite intercambiar artículos que ya no usas
                                     u ofrecer tus servicios a cambio de aquellos otros servicios o artículos que necesitas o quieres, 
                                     <span className="text-primary fw-bold"> sin gastar dinero. </span>
                                     Para poder ofrecer productos o servicios y realizar intercambios deberás estár registrado en la página.
