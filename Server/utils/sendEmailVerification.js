b377608b4f4f9ecb5f834f5917e514976fee0a8a
Fabian Castro <facaslo.99@gmail.com>
2022-06-28 21:19:43 -0500

    Se han actualizado las credenciales de nodemailer

--- Server/utils/sendEmailVerification.js
+++ Server/utils/sendEmailVerification.js
@@ -1,30 +1,57 @@
 const nodemailer = require('nodemailer');
+const {google} = require('googleapis')
 
-// Información del email de origen
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'tausch.email.token@gmail.com',
-      pass: 'ytRg82ZJWPfgiLb'
-    }
-  });
+const CLIENT_ID = '456307078252-76g5t7kt27rd1j1oobk3v10rduj24jku.apps.googleusercontent.com'
+const CLIENT_SECRET = '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'
+const REDIRECT_URI = 'https://developers.google.com/oauthplayground'
+const REFRESH_TOKEN = '1//04pkbyPd8jXqICgYIARAAGAQSNwF-L9Ir3Dx_rT31rK5mwvlMNY4hpa2vlT1eEqcCcAmSWSx5KG19wRA2flpKAHJCcATk0mRp18c'
+
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
+oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN})
+
+
 
 const sendActivationEmail = async (user, token, email) => {
+    const accessToken = await oAuth2Client.getAccessToken()
+
+    console.log('entra')
+    // Información del email de origen
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+        type: 'OAuth2',
+        user: 'tausch.email.token@gmail.com ',
+        clientId : CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        refreshToken: REFRESH_TOKEN,
+        accessToken : accessToken
+        } 
+    })
     
     if(typeof email === 'string' && email.length > 0){
-        let emailOptions = {
-            from: 'tausch.email.token@gmail.com',
-            to: email,
-            subject: 'Verificación de cuenta de Tausch',            
-            html: `<p> Para activar su cuenta de Tausch acceda al siguiente enlace <hr/> <a href="http://localhost:3000/activacion?user=${user}&token=${token}"> enlace de activación </a>`
+        try{
+            let emailOptions = {
+                from: 'tausch.email.token@gmail.com',
+                to: email,
+                subject: 'Verificación de cuenta de Tausch',            
+                html: `<p> Para activar su cuenta de Tausch acceda al siguiente enlace <hr/> <a href="http://localhost:3000/activacion?user=${user}&token=${token}"> enlace de activación </a>`
+            }
+    
+            const result = await transporter.sendMail(emailOptions);
+            await console.log(result)
         }
-
-        await transporter.sendMail(emailOptions);
+        catch (e){
+            console.log(e)
+        }        
     }
-    else{
-        console.log("Error al enviar email");        
-    }
+    
 }
+
+
+// (async function(){
+//     await sendActivationEmail('facaslo', 'hola ',  'facaslo.99@gmail.com');
+// })();
+
 
 
 module.exports = sendActivationEmail
